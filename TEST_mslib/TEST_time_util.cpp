1cffa8fbe8e519948a2fff366dc1b3ecc567fc6e
rla523at <rla523at@naver.com>
2025-08-30 11:27:44 +0900

    auto commit

--- TEST_mslib/TEST_time_util.cpp
+++ TEST_mslib/TEST_time_util.cpp
@@ -39,3 +39,56 @@ TEST( time_util, 1 )
   EXPECT_TRUE( time_recorder1._count == 1 );
   EXPECT_TRUE( time_recorder2._count == N );
 }
+
+TEST( time_util, 2 )
+{
+  constexpr int N = 100000;
+
+  std::vector<double> value( N );
+  for ( int i = 0; i < N; ++i )
+  {
+    value[i] = static_cast<double>( i );
+  }
+
+  double              sum  = 0;
+  double              sum2 = 0;
+  std::vector<double> temp;
+  std::vector<double> temp2;
+
+  Time_Recorder time_recorder1;
+  {
+    {
+      RECORD_THIS_SCOPE( time_recorder1 );
+      for ( int i = 0; i < N; ++i )
+      {
+         //RECORD_THIS_SCOPE( time_recorder1 );
+        temp.push_back( value[i] );
+      }
+    }
+
+    sum += temp[1234];
+  }
+
+  Time_Recorder time_recorder2;
+  {
+    temp2.reserve( N );
+
+    {
+      RECORD_THIS_SCOPE( time_recorder2 );
+      for ( int i = 0; i < N; ++i )
+      {
+        //RECORD_THIS_SCOPE( time_recorder2 );
+        temp2.push_back( value[i] );
+      }
+    }
+
+    sum2 += temp2[1234];
+  }
+  std::cout << time_recorder1._total_time << "\n";
+  std::cout << time_recorder2._total_time << "\n";
+
+  EXPECT_EQ( sum, sum2 );
+
+  // EXPECT_TRUE( time_recorder1._count == 1 );
+  // EXPECT_TRUE( time_recorder2._count == N );
+}
--- _src/time_util.h
+++ _src/time_util.h
@@ -2,16 +2,47 @@
 
 namespace mslib::time_util
 {
-  using Time_Point = std::chrono::steady_clock::time_point;
-  using NanoSecond = std::chrono::nanoseconds;
-  using MillSecond = std::chrono::duration<double, std::milli>;
+  using Clock_Type = std::chrono::steady_clock;
+  using Time_Point = Clock_Type::time_point;
+  using Nano_Second = std::chrono::nanoseconds;
+  using Mill_Second = std::chrono::duration<double, std::milli>;
 
   constexpr Time_Point Invalid_Time_Point = Time_Point();
 
+  inline Time_Point get_time_point( void )
+  {
+    return Clock_Type::now();
+  }
+  inline uint64 calculate_duration_ns( const Time_Point& tp1, const Time_Point& tp2 )
+  {
+    Nano_Second duration = tp2 - tp1;
+    return static_cast<uint64>( duration.count() );
+  }
+  inline double cal_ellapsed_time_ms( const Time_Point& tp1, const Time_Point& tp2 )
+  {
+    Nano_Second duration    = tp2 - tp1;
+    Mill_Second duration_ms = std::chrono::duration_cast<Mill_Second>( duration );
+    return duration_ms.count();
+  }
+
   struct Time_Recorder
   {
-    void set_start_time_point( void );
-    void record( void );
+    inline void set_start_time_point(void)
+    {
+      _start_time_point = get_time_point();
+    }
+    inline void record(void)
+    {
+      const Time_Point end_time_point = get_time_point();
+
+      const double ellapsed_time = cal_ellapsed_time_ms( _start_time_point, end_time_point );
+
+      _count      += 1;
+      _total_time += ellapsed_time;
+      _min_time    = std::min( _min_time, ellapsed_time );
+      _max_time    = std::max( _max_time, ellapsed_time );
+      _avg_time    = _total_time / _count;  
+    }
 
     uint64     _count            = 0;
     double     _total_time       = 0;
